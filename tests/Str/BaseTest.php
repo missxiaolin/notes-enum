11f4e42ba567aabea8f8fde38bc737edbbc2cdf1
xiaolin <462441355@qq.com>
2018-03-24 14:38:16 +0800

    * 初始化目录

---
+++ tests/Str/BaseTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Str;
+// +----------------------------------------------------------------------
+// | BaseTest.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 xiaolin All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: xiaolin <462441355@qq.com> <https://github.com/missxiaolin>
+// +----------------------------------------------------------------------
+
+
+use PHPUnit\Framework\TestCase;
+use Tests\App\ErrorCode;
+
+class BaseTest extends TestCase
+{
+
+}
